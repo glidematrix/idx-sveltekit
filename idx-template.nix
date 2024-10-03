ae7bd44f217f45808d3a6b749b935aaeb91f27e4
glidematrix <mundiamwala@gmail.com>
2024-10-04 01:53:39 +0200

    update

--- idx-template.nix
+++ idx-template.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   packages = [
-    pkgs.nodejs_24
+    pkgs.nodejs_22
   ];
   bootstrap = ''    
     npm create svelte@latest \"$WS_NAME\" -- --template skeleton --typescript --eslint --prettier
