c712f69204abfd2f531364f1752191fea080b528
glidematrix <mundiamwala@gmail.com>
2024-10-04 01:33:29 +0200

    first commit

---
+++ idx-templete.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }: {
+  packages = [
+    pkgs.nodejs_24
+  ];
+  bootstrap = ''    
+    npm create svelte@latest \"$WS_NAME\" -- --template skeleton --typescript --eslint --prettier
+
+    mkdir -p "$WS_NAME/.idx/"
+    cp -rf ${./dev.nix} "$WS_NAME/.idx/dev.nix"
+    chmod -R +w "$WS_NAME"
+
+    mv "$WS_NAME" "$out"
+    cd "$out"; npm install --package-lock-only --ignore-scripts
+
+  '';
+}
